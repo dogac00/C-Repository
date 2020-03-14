64c3f5447b06505dd7e6ef53e3ff89dea7cf6293
Dogac Akyıldız <ndakyildiz@gmail.com>
2020-03-15 01:58:39 +0300

    We can swap the values, no need to take addresses.

--- macro-swap.c
+++ macro-swap.c
@@ -1,16 +1,17 @@
 
 #define swap_int(first, second) \
 { \
-	int temp = first; \
-	*(first) = *(second); \
-	*(second) = temp; \
+	int temp = (first); \
+	(first) = second; \
+	(second) = temp; \
 }
+#include <stdio.h>
 
 int main()
 {
 	int a = 5, b = 15;
-	swap_int(&a, &b);
+	swap_int(a, b);
 
 	int c = 25, d = 35;
-	swap_int(&c, &d);
+	swap_int(c, d);
 }
