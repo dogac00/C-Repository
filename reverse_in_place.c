0fc6bd2db9fff25831863aac283cde140fd828f3
Dogac Akyıldız <ndakyildiz@gmail.com>
2020-03-15 16:39:21 +0300

    Remove implementation defined behavior.

--- reverse_in_place.c
+++ reverse_in_place.c
@@ -1,20 +1,19 @@
-
 #include <string.h>
 #include <stdio.h>
 
-char* reverse_in_place(char str[]);
+char* reverse_in_place(char* str);
 
 int main()
 {
 	char tmp[] = "World";
 	printf("%s", reverse_in_place(tmp));
-	
+
 	return 0;
 }
 
-char* reverse_in_place(char str[])
+char* reverse_in_place(char* str)
 {
-	const int length = strlen(str); // Implementation-defined
+	const unsigned length = strlen(str);
 
 	for (int i = 0; i < length / 2; i++)
 	{
