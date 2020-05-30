ac87cba8370149fba1461b3802f616c7c7ce9bc6
Dogac Akyıldız <ndakyildiz@gmail.com>
2020-05-30 22:57:22 +0300

    Update and rename macro-swap.c to generic_swap.c

---
+++ generic_swap.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+void generic_swap(void* p1, void* p2, size_t elemSize)
+{
+	char* ch1 = p1;
+	char* ch2 = p2;
+	
+	while (elemSize--)
+	{
+		const char tmp = *ch1;
+		*ch1++ = *ch2;
+		*ch2++ = tmp;
+	}
+}
+
+int main()
+{
+	int a = 5, b = 15;
+	printf("a = %d, b = %d\n", a, b);
+	generic_swap(&a, &b, sizeof(int));
+	printf("a = %d, b = %d\n", a, b);
+
+	double c = 25.54436, d = 35.7879765;
+	printf("c = %f, d = %f\n", c, d);
+	generic_swap(&c, &d, sizeof(double));
+	printf("c = %f, d = %f\n", c, d);
+}
