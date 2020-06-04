d8b8ed979ed810228a1fbfbd554700af31b6a444
Dogac Akyıldız <ndakyildiz@gmail.com>
2020-06-04 17:41:24 +0300

    Create any_odd_bit.c

---
+++ any_odd_bit.c
@@ -0,0 +1,17 @@
+
+// I solved this question on codewars.com
+
+// check whether any odd bit of x is 1
+// it should return 1 if odd bit equal to 1 exists, 0 otherwise
+
+int any_odd(unsigned x) {
+  int cnt = 0;
+  
+  while (x) {
+    if (cnt++ % 2 == 1 && (x & 1))
+      return 1;
+    x >>= 1;
+  }
+  
+  return 0;
+}
