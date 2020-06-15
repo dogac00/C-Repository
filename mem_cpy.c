c1ecb1a455feb10ed3990220faefd1b418b72558
Dogac Akyıldız <ndakyildiz@gmail.com>
2020-06-15 21:08:39 +0300

    Update mem_cpy.c

--- mem_cpy.c
+++ mem_cpy.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-char* mem_cpy(void* dest, const void* src, size_t n)
+char* mymemcpy1(void* dest, const void* src, size_t n)
 {
 	char* d = (char*) dest;
 	const char* s = (const char*) src;
