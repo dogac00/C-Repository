9420e8fcc3c1e308ec9898be5ab5823e5dac0689
Dogac Akyıldız <ndakyildiz@gmail.com>
2020-06-28 04:31:17 +0300

    Update mypow.c

--- mypow.c
+++ mypow.c
@@ -7,6 +7,12 @@ the idea behind it is 4 ^ 4 = 16 ^ 2 and 3 ^ 5 = 3 * 9 ^ 2
 we make base bigger and exp smaller and we end up in the base case sooner or later
 I will be committing an iterative implementation to save stack space
 
+----------------------
+
+Added the iterative_pow implemenation too,
+of course we can calculate the power by using a simple for loop
+to 1 to n but I think this is faster because we loop log(N) time
+
 *******************************************************************************/
 
 #include <stdio.h>
@@ -24,6 +30,27 @@ double pow(double base, int exp) {
     
     return pow(base * base, exp / 2);
 }
+
+
+double iterative_pow(double base, int exp) {
+    if (exp < 0) {
+        base = 1 / base;
+        exp = -exp;
+    }
+    
+    double result = base;
+    
+    while (exp > 1) {
+        if (exp % 2)
+            result *= base * result;
+        else
+            result *= result;
+        exp /= 2;
+    }
+    
+    return result;
+}
+
 
 int main()
 {
