0abc59abbeee5687a4b1755d08ccc037bfe33eff
Dogac Akyıldız <ndakyildiz@gmail.com>
2020-06-29 06:33:38 +0300

    Update assert_macro.c

--- assert_macro.c
+++ assert_macro.c
@@ -1,19 +1,11 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 #include <assert.h>
 
 int main()
 {
-	const char func[] = __FUNCTION__;
-
-	assert(strcmp(func, "main") == 0);
-	
-	puts(func);
+	assert(!strcmp(__FUNCTION__, "main"));
 	
 	return 0;
 }
-
-// of course we know the function name is main
-// but the aim is to show the assert macro
