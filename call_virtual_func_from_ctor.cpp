98f711c3bc8b106d432b1e5eb5007e5df6069390
Dogac Akyıldız <ndakyildiz@gmail.com>
2020-08-27 20:19:57 +0300

    Update call_virtual_func_from_ctor.cpp

--- call_virtual_func_from_ctor.cpp
+++ call_virtual_func_from_ctor.cpp
@@ -5,7 +5,7 @@ class Base
 {
   Base()
   {
-    foo(); // Call to foo here will be statically resolved
+    foo(); // The derived object is not alive, therefore call to foo here will be statically resolved
   }
   
   virtual void foo() 
